0b2ca68594c9094e0307b3cdeec70c687199cf5a
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-04 17:22:57 -0600

    Container setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { HomeSectionComponent } from "./home-section/home-section.component";
+import { AboutSectionComponent } from "./about-section/about-section.component";
+import { ProjectsSectionComponent } from "./projects-section/projects-section.component";
+import { ContactSectionComponent } from "./contact-section/contact-section.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeSectionComponent,
+    AboutSectionComponent,
+    ProjectsSectionComponent,
+    ContactSectionComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
