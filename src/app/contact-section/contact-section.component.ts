e2a75bbf0c9bbdc6839253daac2a759a4ac3ecae
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-04 17:21:21 -0600

    Completed contact-section component

---
+++ src/app/contact-section/contact-section.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+declare var Rellax: any;
+@Component({
+  selector: "app-contact-section",
+  templateUrl: "./contact-section.component.html",
+  styleUrls: ["./contact-section.component.css"],
+})
+export class ContactSectionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    // var rellax = new Rellax(".rellax");
+  }
+}
