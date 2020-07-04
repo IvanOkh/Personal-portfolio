1a6e6236a8898768b9a4136ebe7c8137bda54a26
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-04 17:22:24 -0600

    Completed about-section component

---
+++ src/app/about-section/about-section.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+declare var Rellax: any;
+@Component({
+  selector: "app-about-section",
+  templateUrl: "./about-section.component.html",
+  styleUrls: ["./about-section.component.css"],
+})
+export class AboutSectionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    // var rellax = new Rellax(".rellax");
+  }
+}
