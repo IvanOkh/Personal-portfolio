215a812e289a4c943958ec2d8048ba4fb1ed72f8
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-04 17:20:34 -0600

    Completed project-section component

---
+++ src/app/projects-section/projects-section.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from "@angular/core";
+
+declare var Rellax: any;
+
+@Component({
+  selector: "app-projects-section",
+  templateUrl: "./projects-section.component.html",
+  styleUrls: ["./projects-section.component.css"],
+})
+export class ProjectsSectionComponent implements OnInit {
+  constructor() {}
+
+  seeContent: boolean = false;
+  seeJavaContent: boolean = false;
+
+  ngOnInit() {
+    // var rellax = new Rellax(".rellax");
+  }
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView();
+  }
+
+  public turn() {
+    if (this.seeContent == false) {
+      this.seeContent = true;
+    } else {
+      this.seeContent = false;
+    }
+  }
+
+  public turnjava() {
+    if (this.seeJavaContent == false) {
+      this.seeJavaContent = true;
+    } else {
+      this.seeJavaContent = false;
+    }
+  }
+}
