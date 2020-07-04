437eec9891a5d773874b6f767bb8a423e1bcb094
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-04 17:21:59 -0600

    Completed header-section component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+declare var Rellax: any;
+@Component({
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    // var rellax = new Rellax(".rellax");
+  }
+}
